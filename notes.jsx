e837d74dd17ee8f98e96dc07732850cd35b0a8a3
gracengure <wnguregrace@gmail.com>
2025-03-03 19:24:10 +0300

    finalize Project

---
+++ notes.jsx
@@ -0,0 +1,10 @@
+//SECTION 19 :Practice project :Building a Food Order App
+//Lecture 1:Formatting and outputting number as currency
+//a)use the Intl.NumberFormat('language', {configuration object }):
+//           i)1st argument is the language to use
+//           ii)2nd argument is the configuration object which has different property as style ,currency
+
+
+//Lecture 2: Getting started with cart context and reducer
+//a)
+//
---
+++ src/components/Meals.jsx
@@ -0,0 +1,22 @@
+
+import MealItem from "./MealItem.jsx";
+import useHttp from "../hooks/useHttp.js";
+import Error from "./Error.jsx";
+const requestConfig={};
+export default function Meals() {
+ const {data:loadedMeals,isLoading , error} =useHttp('https://food-order-app-backend-su0l.onrender.com/meals',requestConfig,[])
+ if(isLoading){
+  return<p className="centre">Fetching meals...</p>
+ }
+ if(error){
+  return <Error title="Failed to fetch meals"  message={error}/>
+ }
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+       <MealItem key={meal.id} meal={meal}/>
+      ))}
+    </ul>
+  );
+}
